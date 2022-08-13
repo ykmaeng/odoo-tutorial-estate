9ab22e68551f92cf634b5793cb1f1853e012c096
young <brosisone@outlook.kr>
2022-08-13 23:02:51 +1200

    add models/

---
+++ models/estate_property.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from odoo import fields, models
+
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Estate Property"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+
+    postcode = fields.Char()
+    date_availability = fields.Date(default=datetime.today())
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection=[('north', 'North'), ('south', 'South'),
+                   ('east', 'East'), ('west', 'West')],
+        help="This is helpful message"
+    )
