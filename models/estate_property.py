cb3868e8c718bf8c1cc2efe9dddf2962cf87d4cf
young <brosisone@outlook.kr>
2022-08-14 21:53:11 +1200

    Remove codes of chapter 9. Fix unknown parameter 'tracking' warnings

--- models/estate_property.py
+++ models/estate_property.py
@@ -29,28 +29,16 @@ class EstateProperty(models.Model):
                    ('east', 'East'), ('west', 'West')],
         help="This is helpful message"
     )
-    total_area = fields.Integer(compute='_compute_total_area')
     state = fields.Selection(
         selection=[('new', 'New'), ('offer_received', 'Offer Received'),
                    ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
         default='new',
         help="This is helpful message"
     )
-    buyer = fields.Many2one('res.users', string='Buyer', index=True,
-                            tracking=True, default=lambda self: self.env.user)
+    buyer = fields.Many2one('res.users', string='Buyer',
+                            index=True, default=lambda self: self.env.user)
     salesperson = fields.Many2one('res.users', string='Salesperson',
-                                  index=True, tracking=True, default=lambda self: self.env.user)
+                                  index=True, default=lambda self: self.env.user)
     offer_ids = fields.One2many(
         'estate.property.offer', 'property_id', string='Offers')
     active = fields.Boolean(default=True)
-
-    # Compute total area
-    @api.depends('living_area', 'garden_area')
-    def _compute_total_area(self):
-        for record in self:
-            record.total_area = record.living_area + record.garden_area
-
-    @api.onchange('garden')
-    def _onchange_garden(self):
-        self.garden_area = 0 if not self.garden else 10
-        self.garden_orientation = False if not self.garden else 'north'
