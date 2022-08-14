4ecfa8beb63138481663a7bc7143c55be1243cc3
young <brosisone@outlook.kr>
2022-08-14 15:59:57 +1200

    add codes for chapter 8

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from odoo import fields, models
+from email.policy import default
+from odoo import api, fields, models
 
 
 class EstateProperty(models.Model):
@@ -7,7 +8,11 @@ class EstateProperty(models.Model):
     _description = "Estate Property"
 
     name = fields.Char(required=True)
+    tag_ids = fields.Many2many("estate.property.tag")
     description = fields.Text()
+
+    property_type_id = fields.Many2one(
+        'estate.property.type', string='Property Type')
 
     postcode = fields.Char()
     date_availability = fields.Date(default=datetime.today(), copy=False)
@@ -24,10 +29,28 @@ class EstateProperty(models.Model):
                    ('east', 'East'), ('west', 'West')],
         help="This is helpful message"
     )
+    total_area = fields.Integer(compute='_compute_total_area')
     state = fields.Selection(
         selection=[('new', 'New'), ('offer_received', 'Offer Received'),
                    ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
         default='new',
         help="This is helpful message"
     )
+    buyer = fields.Many2one('res.users', string='Buyer', index=True,
+                            tracking=True, default=lambda self: self.env.user)
+    salesperson = fields.Many2one('res.users', string='Salesperson',
+                                  index=True, tracking=True, default=lambda self: self.env.user)
+    offer_ids = fields.One2many(
+        'estate.property.offer', 'property_id', string='Offers')
     active = fields.Boolean(default=True)
+
+    # Compute total area
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        self.garden_area = 0 if not self.garden else 10
+        self.garden_orientation = False if not self.garden else 'north'
